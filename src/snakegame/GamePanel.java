f1b9ee0494fcc6bf88e4f3f7725ebac01c53519c
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2023-01-15 09:44:06 -0300

    Update GamePanel.java

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -8,15 +8,37 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
-    static final  int SCREEN_WIDTH = 600;
-    static final  int SCREEN_HEIGHT = 600;
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
-    
+
     GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
 
     }
 
     public void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY)
 
     }
 
@@ -25,6 +47,11 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g) {
+
+    }
+
+
+    public void newApple() {
 
     }
 

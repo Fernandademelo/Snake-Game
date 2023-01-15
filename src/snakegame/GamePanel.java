fd922d5a52b022fc286b960f9388ca223a72b00f
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2023-01-15 00:50:21 -0300

    Snake-Game
    first commit

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -1,16 +1,59 @@
 package snakegame;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
 
 public class GamePanel extends JPanel implements ActionListener {
+
+    static final  int SCREEN_WIDTH = 600;
+    static final  int SCREEN_HEIGHT = 600;
+
+    
     GamePanel() {
 
     }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisons() {
+
+    }
+
+    public void gameOver() {
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
 
 }
